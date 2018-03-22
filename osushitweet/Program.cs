8f4adfde1609b8fd2d9883d37bbdff87eab24805
mass10 <mass10.github@gmail.com>
2018-03-22 23:00:37 +0900

    wip

---
+++ osushitweet/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoreTweet;
+
+namespace osushitweet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tokens = CoreTweet.Tokens.Create(
+                "XXXXXXXXXXXXXXXXXXXXXXXXX",
+                "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+                "999999999999999999-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+                "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            var text = "hello #test";
+            tokens.Statuses.Update(new { status = text });
+        }
+    }
+}
